0fb11f34ce57a836db334f42417fa2f296569230
Chokkakula Padma Pravalika <chokkakulapadmapravalika123@gmail.com>
2025-09-07 00:02:46 +0530

    Update frontend environment.js for production

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,11 +1,11 @@
-// environment.js
+// frontend/src/environment.js
 
 // Check if React is running in production
 const IS_PROD = process.env.NODE_ENV === "production";
 
-// Use the backend URL from environment variable for production, fallback to localhost
+// Backend URL
 const server = IS_PROD
-  ? process.env.REACT_APP_API_URL || "https://proconnect-x0ok.onrender.com" // Production backend
-  : "http://localhost:8000"; // Local backend
+  ? process.env.REACT_APP_API_URL || "https://proconnect-x0ok.onrender.com"
+  : "http://localhost:8000";
 
 export default server;
