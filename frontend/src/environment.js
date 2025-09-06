6fe38ee6ef02bf344e59c92b14d26d76797b0f86
Chokkakula Padma Pravalika <chokkakulapadmapravalika123@gmail.com>
2025-09-06 23:21:52 +0530

    Update environment.js for production and development URLs

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,7 +1,10 @@
-const IS_PROD = false;
+// environment.js
+
+// Check if the environment variable NODE_ENV is set to "production"
+const IS_PROD = process.env.NODE_ENV === "production";
 
 const server = IS_PROD
-  ? "https://apnacollegebackend.onrender.com" // production
-  : "http://localhost:8000"; // local backend
+  ? "https://proconnect-x0ok.onrender.com" // Production backend
+  : "http://localhost:8000";               // Local backend for development
 
 export default server;
